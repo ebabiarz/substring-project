3a67af1e524137ae934b398869f885b01f212887
Eric Babiarz <145168964+ebabiarz@users.noreply.github.com>
2024-10-26 19:00:38 -0500

    Create #substring method that compares a string to an array of strings and returns a hash with each matching string as the key and the number of occurrences as the value.

--- substring.rb
+++ substring.rb
@@ -1,1 +1,25 @@
-# This project hasn't been started yet
+
+def substrings(string, dictionary)
+  matches = Array.new()
+  dictionary.each do |word|
+    string.split(' ').each do |segment|
+      if segment.downcase.include?(word.downcase) == true
+        matches.push(word)
+      end
+    end
+  end
+  return matched_words(matches)
+end
+
+def matched_words(matches)
+  matches.reduce(Hash.new(0)) do |word, occurance|
+    word[occurance] += 1
+    word
+  end
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("below", dictionary)
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
